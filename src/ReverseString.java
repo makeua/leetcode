af5cf5597e7e857b8175b1343e9ecb8b5e5cb0e1
Maksym Tkachov <maksym.tkachov@sigma.software>
2023-02-07 16:15:44 +0100

    5

---
+++ src/ReverseString.java
@@ -0,0 +1,12 @@
+public class ReverseString {
+    public void reverseString(char[] s) {
+        int l = 0, r = s.length - 1;
+        while (l < r) {
+            char t = s[l];
+            s[l] = s[r];
+            s[r] = t;
+            l++;
+            r--;
+        }
+    }
+}
